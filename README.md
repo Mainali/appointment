8bfcd7b14f81d93ffc2e2bd287b195272c734cf4
Pukar Mainali <mainalipukar@gmail.com>
2017-09-26 21:19:33 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,12 @@
+**Appointment application**
+
+
+
+_Installing and Running_
+
+Clone / download the project in Server directory.
+Change the shebang line in app.pl according to the perl installation directory
+Change the redirect url and database credentials inside app.pl
+
+
+ 
---
+++ app/app.pl
@@ -0,0 +1,70 @@
+#!C:\Perl64\bin\perl.exe
+
+    use CGI;
+ 	use strict;
+  	use warnings;
+  	use DBI;
+  	use Data::Dumper;
+    use JSON;
+    use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+
+    print CGI::header();
+    my $cgi = CGI->new;
+
+        #get all the cgi request parameters
+    	my $date = $cgi->param("date");
+    	my $time = $cgi->param("time");
+    	my $description = $cgi->param("description");
+    	my $search = $cgi->param("search");
+    	if(not defined $search){
+    	    $search = "";
+    	}
+
+        #create table if it doesn't exists
+        createTable();
+
+        #execute insert and select based on request parameters (if there are form inputs then execute insert and redirect, else return JSON based on search text)
+    	if(defined $date and defined $time and defined $description){
+            my $db = connect_db();
+            my $sql = 'insert into appointments (date, time, description) values (?, ?, ?)';
+            my $sth = $db->prepare($sql) or die $db->errstr;
+            $sth->execute(
+               $date,
+               $time,
+               $description
+            ) or die $sth->errstr;
+
+             # redirect to application page
+
+             my $url="http://localhost:8080/appointment/index.html";
+             my $t=0; # time until redirect activates
+             print "<META HTTP-EQUIV=refresh CONTENT=\"$t;URL=$url\">\n";
+
+    	}else{
+            my $db = connect_db();
+            my $sql = "select date, time, description from appointments WHERE description LIKE '%$search%' order by date desc;";
+            my $sth = $db->prepare($sql) or die $db->errstr;
+            $sth->execute or die $sth->errstr;
+
+           my @output;
+
+             while (my $row = $sth->fetchrow_hashref) {
+                push @output, $row;
+             }
+
+             print to_json(\@output);
+    	}
+
+
+#subroutine for database connection
+  	sub connect_db {
+          my $dbh = DBI->connect("dbi:SQLite:dbname=appointments.db") or
+          die $DBI::errstr;
+          return $dbh;
+        }
+
+#subroutine for creating table if it doesnt exists
+    sub createTable{
+                my $db = connect_db();
+                $db->do('CREATE TABLE IF NOT EXISTS appointments (id INTEGER PRIMARY KEY ,date TEXT, time TEXT, description TEXT NOT NULL)');
+            }
